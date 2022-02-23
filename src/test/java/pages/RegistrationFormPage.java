55b294a3c09edc2b5ef27a487beae24b09f49fff
vladimir <negovan.djurovic@zamia.be>
2022-02-23 15:55:18 +0100

    I added selectors

--- src/test/java/pages/RegistrationFormPage.java
+++ src/test/java/pages/RegistrationFormPage.java
@@ -1,4 +1,33 @@
 package pages;
 
-public class RegistrationFormPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class RegistrationFormPage extends BasePage{
+    public RegistrationFormPage(WebDriver driver, WebDriverWait wait) {
+        super(driver, wait);
+        this.driver = driver;
+        this.wait = wait;
+        PageFactory.initElements(driver,this);
+    }
+    @FindBy(css = "input[id*='txtUserName']")
+    WebElement username;
+    @FindBy(css = "input[id*='txtEmail']")
+    WebElement emailAddress;
+    @FindBy(xpath = "//input[@id=\"ContentPlaceHolder1_txtPassword\"]")
+    WebElement password;
+    @FindBy(css = "input[id*='txtPassword2']")
+    WebElement confirmPassword;
+    @FindBy(css = "input[id*='MyCheckBox']")
+    WebElement termsAndConditions;
+    @FindBy(css = "input[id*='SubscribeNewsletter']")
+    WebElement subscribeNewsletter;
+    @FindBy(css = "input[id*='btnRegister']")
+    WebElement createAnAccountButton;
+
+
+
 }
