7becacfff001eb7ec9f7ade7bae861ea2b2c4074
vladimir <negovan.djurovic@zamia.be>
2022-02-23 17:05:08 +0100

    Added methods on RegistrationFormPage

--- src/test/java/pages/RegistrationFormPage.java
+++ src/test/java/pages/RegistrationFormPage.java
@@ -1,9 +1,13 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class RegistrationFormPage extends BasePage{
@@ -27,6 +31,39 @@ public class RegistrationFormPage extends BasePage{
     WebElement subscribeNewsletter;
     @FindBy(css = "input[id*='btnRegister']")
     WebElement createAnAccountButton;
+
+    public void enterUsername(String usernameText){
+        username.sendKeys(usernameText);
+    }
+    public void enterEmailAddress(String emailText){
+        emailAddress.sendKeys(emailText);
+    }
+    public void enterPassword(String passwordText){
+        password.sendKeys(passwordText);
+    }
+    public void enterConfirmPassword(String confirmPasswordText){
+        confirmPassword.sendKeys(confirmPasswordText);
+    }
+    public void checkBoxTermsAndConditions(){
+        termsAndConditions.click();
+    }
+    public void checkBoxSubscribeNewsletter(){
+        subscribeNewsletter.click();
+    }
+    public void clickCreateAnAccountButton(){
+        createAnAccountButton.click();
+    }
+    public void fillRegistrationForm(String usernameText, String emailText, String passwordText, String confirmPasswordTex ){
+        enterUsername(usernameText);
+        enterEmailAddress(emailText);
+        enterPassword(passwordText);
+        enterConfirmPassword(confirmPasswordTex);
+        checkBoxTermsAndConditions();
+        checkBoxSubscribeNewsletter();
+        clickCreateAnAccountButton();
+
+    }
+
 
 
 
--- src/test/java/tests/RegistrationFormTest.java
+++ src/test/java/tests/RegistrationFormTest.java
@@ -1,7 +1,11 @@
 package tests;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -13,9 +17,14 @@ public class RegistrationFormTest {
     public void setUp(){
 
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver98.exe");
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        options.addArguments("disable-infobars");
+        options.addArguments("--disable-extensions");
+        WebDriver driver = new ChromeDriver(options);
         driver.get(URL);
+        new WebDriverWait(driver, 10).until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(By.xpath("//iframe[starts-with(@name, 'a-') and starts-with(@src, 'https://www.google.com/recaptcha')]")));
+        new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.cssSelector("div.recaptcha-checkbox-checkmark"))).click();
     }
     @AfterMethod
     public void tearDown(){
