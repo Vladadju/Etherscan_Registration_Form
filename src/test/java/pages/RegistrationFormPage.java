2c8459fe9672dacf06ed7bd86cd9ef1b4a1bae32
vladimir <negovan.djurovic@zamia.be>
2022-02-24 09:51:38 +0100

    Added tests on RegistrationFormtest class

--- src/test/java/pages/RegistrationFormPage.java
+++ src/test/java/pages/RegistrationFormPage.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -21,19 +22,41 @@ public class RegistrationFormPage extends BasePage{
     WebElement username;
     @FindBy(css = "input[id*='txtEmail']")
     WebElement emailAddress;
-    @FindBy(xpath = "//input[@id=\"ContentPlaceHolder1_txtPassword\"]")
+    @FindBy(xpath = "//input[@id='ContentPlaceHolder1_txtPassword']")
     WebElement password;
     @FindBy(css = "input[id*='txtPassword2']")
     WebElement confirmPassword;
-    @FindBy(css = "input[id*='MyCheckBox']")
+    @FindBy(css = "label[for='ContentPlaceHolder1_MyCheckBox']")
     WebElement termsAndConditions;
-    @FindBy(css = "input[id*='SubscribeNewsletter']")
+    @FindBy(css = "label[for='ContentPlaceHolder1_SubscribeNewsletter']")
     WebElement subscribeNewsletter;
     @FindBy(css = "input[id*='btnRegister']")
     WebElement createAnAccountButton;
+    @FindBy(css = "#ContentPlaceHolder1_txtUserName-error")
+    WebElement invalidMessageForUsername;
+    @FindBy(css = "#ContentPlaceHolder1_txtEmail-error")
+    WebElement invalidMessageForEmail;
+    @FindBy(xpath = "//*[@id='ContentPlaceHolder1_txtPassword-error']")
+    WebElement invalidMessageForPassword;
+    @FindBy(css = "div[id*='txtPassword2']")
+    WebElement invalidMessageForConfirmPassword;
+    @FindBy(css = "div[id='ctl00$ContentPlaceHolder1$MyCheckBox-error']")
+    WebElement invalidMessageForTermsAndConditions;
+    @FindBy(xpath = "//*[@id='btnCookie']")
+    WebElement acceptCookies;
+    @FindBy(css = "iframe[name='a-pmu4asq3bpy9")
+    WebElement iFrame;
+    @FindBy(xpath = "//*[@id='recaptcha-anchor']/div[4]")
+    WebElement reCaptcha;
+    String expectedErrorMessageForUsername = "Username is invalid.";
+    String expectedErrorMessageForEmail = "Please enter a valid email address.";
+    String expectedErrorMessageForPassword = "Your password must be at least 5 characters long.";
+    String expectedErrorMessageForConfirmPassword = "Your password must be at least 5 characters long.";
+    String expectedErrorMessageForTermsAndConditions = "Please accept our Terms and Conditions.";
+
 
     public void enterUsername(String usernameText){
-        username.sendKeys(usernameText);
+       username.sendKeys(usernameText);
     }
     public void enterEmailAddress(String emailText){
         emailAddress.sendKeys(emailText);
@@ -51,8 +74,35 @@ public class RegistrationFormPage extends BasePage{
         subscribeNewsletter.click();
     }
     public void clickCreateAnAccountButton(){
-        createAnAccountButton.click();
+       createAnAccountButton.click();
     }
+    public void verifyErrorMessageUsername(){
+        assertEquals(invalidMessageForUsername,expectedErrorMessageForUsername);
+    }
+    public void verifyErrorMessageEmail(){
+        assertEquals(invalidMessageForEmail, expectedErrorMessageForEmail);
+    }
+    public void verifyErrorMessageForPassword(){
+        assertEquals(invalidMessageForPassword, expectedErrorMessageForPassword);
+    }
+    public void verifyErrorMessageForConfirmPassword(){
+        assertEquals(invalidMessageForConfirmPassword, expectedErrorMessageForConfirmPassword);
+    }
+    public void verifyErrorMessageForTermsAndConditions(){
+        assertEquals(invalidMessageForTermsAndConditions, expectedErrorMessageForTermsAndConditions);
+    }
+    public void clickOnAcceptCookiesButton(){
+        click(acceptCookies);
+    }
+    public void clickOnReCaptcha() {
+        WebDriverWait wait = new WebDriverWait(driver, 30);
+        wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(By.xpath("//iframe[starts-with(@name,'a-')]")));
+        WebElement element = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("div.recaptcha-checkbox-checkmark")));
+        Actions actions = new Actions(driver);
+        actions.moveToElement(element).click();
+        driver.switchTo().defaultContent();
+    }
+
     public void fillRegistrationForm(String usernameText, String emailText, String passwordText, String confirmPasswordTex ){
         enterUsername(usernameText);
         enterEmailAddress(emailText);
--- src/test/java/tests/RegistrationFormTest.java
+++ src/test/java/tests/RegistrationFormTest.java
@@ -1,37 +1,148 @@
 package tests;
-
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.RegistrationFormPage;
 
 public class RegistrationFormTest {
     WebDriver driver;
+    WebDriverWait wait;
     String URL = "https://etherscan.io/register";
 
     @BeforeMethod
     public void setUp(){
 
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver98.exe");
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("start-maximized");
-        options.addArguments("disable-infobars");
-        options.addArguments("--disable-extensions");
-        WebDriver driver = new ChromeDriver(options);
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
         driver.get(URL);
-        new WebDriverWait(driver, 10).until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(By.xpath("//iframe[starts-with(@name, 'a-') and starts-with(@src, 'https://www.google.com/recaptcha')]")));
-        new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.cssSelector("div.recaptcha-checkbox-checkmark"))).click();
+
     }
     @AfterMethod
     public void tearDown(){
         driver.quit();
     }
+    // Test scenario 01
+    @Test
+    public void skipAllFields(){
+        RegistrationFormPage registrationFormPage = new RegistrationFormPage(driver, wait);
+        registrationFormPage.clickOnAcceptCookiesButton();
+        registrationFormPage.clickCreateAnAccountButton();
+        registrationFormPage.verifyErrorMessageUsername();
+        registrationFormPage.verifyErrorMessageEmail();
+        registrationFormPage.verifyErrorMessageForPassword();
+        registrationFormPage.verifyErrorMessageForConfirmPassword();
+        registrationFormPage.verifyErrorMessageForTermsAndConditions();
 
 
+    }
+    // Test scenario 02
+    @Test
+    public void leaveUsernameFieldEmpty() {
+        RegistrationFormPage registrationFormPage = new RegistrationFormPage(driver, wait);
+        registrationFormPage.clickOnAcceptCookiesButton();
+        registrationFormPage.clickOnReCaptcha();
+        registrationFormPage.enterEmailAddress("vladadjurovic@gmail.com");
+        registrationFormPage.enterPassword("vladadju87");
+        registrationFormPage.enterConfirmPassword("vladadju87");
+        registrationFormPage.checkBoxTermsAndConditions();
+        registrationFormPage.checkBoxSubscribeNewsletter();
+        registrationFormPage.clickCreateAnAccountButton();
+        registrationFormPage.verifyErrorMessageUsername();
+
+    }
+    // Test scenario 03
+    @Test
+    public void usernameLessThan5Characters(){
+        RegistrationFormPage registrationFormPage = new RegistrationFormPage(driver, wait);
+        registrationFormPage.clickOnAcceptCookiesButton();
+        registrationFormPage.clickOnReCaptcha();
+        registrationFormPage.enterUsername("vlad");
+        registrationFormPage.enterEmailAddress("vladadjurovic@gmail.com");
+        registrationFormPage.enterPassword("vladadju87");
+        registrationFormPage.enterConfirmPassword("vladadju87");
+        registrationFormPage.checkBoxTermsAndConditions();
+        registrationFormPage.checkBoxSubscribeNewsletter();
+        registrationFormPage.clickCreateAnAccountButton();
+        registrationFormPage.verifyErrorMessageUsername();
+    }
+    // Test scenario 04
+    @Test
+    public void usernameNonAlphanumericCharacters(){
+        RegistrationFormPage registrationFormPage = new RegistrationFormPage(driver, wait);
+        registrationFormPage.clickOnAcceptCookiesButton();
+        registrationFormPage.clickOnReCaptcha();
+        registrationFormPage.enterUsername("vladimirdju?");
+        registrationFormPage.enterEmailAddress("vladadjurovic@gmail.com");
+        registrationFormPage.enterPassword("vladadju87");
+        registrationFormPage.enterConfirmPassword("vladadju87");
+        registrationFormPage.checkBoxTermsAndConditions();
+        registrationFormPage.checkBoxSubscribeNewsletter();
+        registrationFormPage.clickCreateAnAccountButton();
+        registrationFormPage.verifyErrorMessageUsername();
+    }
+    //Test scenario 05
+    @Test
+    public void leaveEmailAddressFieldEmpty(){
+        RegistrationFormPage registrationFormPage = new RegistrationFormPage(driver, wait);
+        registrationFormPage.clickOnAcceptCookiesButton();
+        registrationFormPage.clickOnReCaptcha();
+        registrationFormPage.enterUsername("vladimirdju");
+        registrationFormPage.enterPassword("vladadju87");
+        registrationFormPage.enterConfirmPassword("vladadju87");
+        registrationFormPage.checkBoxTermsAndConditions();
+        registrationFormPage.checkBoxSubscribeNewsletter();
+        registrationFormPage.clickCreateAnAccountButton();
+        registrationFormPage.verifyErrorMessageEmail();
+    }
+    //Test scenario 06
+    @Test
+    public void invalidEmailAddress(){
+        RegistrationFormPage registrationFormPage = new RegistrationFormPage(driver, wait);
+        registrationFormPage.clickOnAcceptCookiesButton();
+        registrationFormPage.clickOnReCaptcha();
+        registrationFormPage.enterUsername("vladimirdju");
+        registrationFormPage.enterEmailAddress("vladadjurovicgmail.com");
+        registrationFormPage.enterPassword("vladadju87");
+        registrationFormPage.enterConfirmPassword("vladadju87");
+        registrationFormPage.checkBoxTermsAndConditions();
+        registrationFormPage.checkBoxSubscribeNewsletter();
+        registrationFormPage.clickCreateAnAccountButton();
+        registrationFormPage.verifyErrorMessageEmail();
+    }
+    //Test scenario 07
+    @Test
+    public void leavePasswordAndConfirmPasswordFieldEmpty(){
+        RegistrationFormPage registrationFormPage = new RegistrationFormPage(driver, wait);
+        registrationFormPage.clickOnAcceptCookiesButton();
+        registrationFormPage.clickOnReCaptcha();
+        registrationFormPage.enterUsername("vladimirdju");
+        registrationFormPage.enterEmailAddress("vladadjurovic@gmail.com");
+        registrationFormPage.checkBoxTermsAndConditions();
+        registrationFormPage.checkBoxSubscribeNewsletter();
+        registrationFormPage.clickCreateAnAccountButton();
+        registrationFormPage.verifyErrorMessageForPassword();
+        registrationFormPage.verifyErrorMessageForConfirmPassword();
+    }
+    //Test scenario 08
+    @Test
+    public void enterLessThan5CharactersInPasswordAndConfirmPasswordField(){
+        RegistrationFormPage registrationFormPage = new RegistrationFormPage(driver, wait);
+        registrationFormPage.clickOnAcceptCookiesButton();
+        registrationFormPage.clickOnReCaptcha();
+        registrationFormPage.enterUsername("vladimirdju");
+        registrationFormPage.enterEmailAddress("vladadjurovic@gmail.com");
+        registrationFormPage.enterPassword("vlad");
+        registrationFormPage.enterConfirmPassword("vlad");
+        registrationFormPage.checkBoxTermsAndConditions();
+        registrationFormPage.checkBoxSubscribeNewsletter();
+        registrationFormPage.clickCreateAnAccountButton();
+        registrationFormPage.verifyErrorMessageForPassword();
+        registrationFormPage.verifyErrorMessageForConfirmPassword();
+    }
 
 
 }
